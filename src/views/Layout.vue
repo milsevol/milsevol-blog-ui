73649f5e5df3fd5cb627261c2cc89f1a1ac71d6d
小虫架构故事 <15013272917@163.com>
2020-05-30 13:39:20 +0800

    修改

---
+++ src/views/Layout.vue
@@ -0,0 +1,63 @@
+<template>
+  <el-container class="layout-container">
+    <el-header class="layout-header">
+      <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+        <el-menu-item index="1">处理中心</el-menu-item>
+        <el-submenu index="2">
+          <template slot="title">我的工作台</template>
+          <el-menu-item index="2-1">选项1</el-menu-item>
+          <el-menu-item index="2-2">选项2</el-menu-item>
+          <el-menu-item index="2-3">选项3</el-menu-item>
+          <el-submenu index="2-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="2-4-1">选项1</el-menu-item>
+            <el-menu-item index="2-4-2">选项2</el-menu-item>
+            <el-menu-item index="2-4-3">选项3</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+        <el-menu-item index="3" disabled>消息中心</el-menu-item>
+        <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+      </el-menu>
+    </el-header>
+    <el-container class="layout-container">
+      <el-main>Main</el-main>
+       <el-aside width="380px">Aside</el-aside>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Layout',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      activeIndex: '1',
+      activeIndex2: '1'
+    }
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.layout-container{
+  .layout-header{
+    padding: 0px;
+  }
+  .layout-container{
+    padding-left: 50px;
+    padding-right: 50px;
+    background-color: cadetblue;
+  }
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,47 @@
+const path = require('path')
+// 当前是否是开发环境
+// const debug = process.env.NODE_ENV !== 'production'
+const name = 'milsevol小虫架构' // 标题
+function resolve (dir) {
+  return path.join(__dirname, dir)
+}
+
+module.exports = {
+  // 部署应用包时的基本URL
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-demo02/' : '/',
+  // 输出文件路径
+  outputDir: 'dist',
+  // 放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录。
+  assetsDir: 'static',
+  // 生成的index.html的路径
+  indexPath: 'index.html',
+  // 生成的静态资源用hash来命名
+  filenameHashing: true,
+  // 当修改完代码之后是否就启动eslint的语法检查
+  lintOnSave: false,
+  // 是否使用包含运行时编译器的Vue构建版本，设置为 true 后你就可以在 Vue 组件中使用 template 选项了，但是这会让你的应用额外增加 10kb 左右。
+  runtimeCompiler: true,
+  // 默认情况下 babel-loader 会忽略所有 node_modules 中的文件。如果你想要通过 Babel 显式转译一个依赖，可以在这个选项中列出来。
+  transpileDependencies: [],
+  // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建。
+  productionSourceMap: false,
+  devServer: {
+    open: true,
+    host: '127.0.0.1',
+    port: 3002,
+    https: false,
+    hotOnly: false,
+    proxy: null
+  },
+  configureWebpack: {
+    name: name,
+    resolve: {
+      alias: {
+        '@': resolve('src')
+      }
+    }
+  },
+  // 第三方插件配置
+  pluginOptions: {}
+
+}
